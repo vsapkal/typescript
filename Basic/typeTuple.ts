21740bdd65fc5bb706cf69fa7923822c7edb0fa0
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-01 18:05:02 +0530

    Tuple type

--- Basic/typeTuple.ts
+++ Basic/typeTuple.ts
@@ -1,3 +1,8 @@
+/* 
+    difference between Tuple and Array.
+    "Tuple" : In tuple you can mention each and every index value variable type .
+    "Array" : In array you have to declare variable type to whole Array.
+ */
 
 let tuple:[string,number,boolean?]=["Alex",23];
 tuple.push("Vinod");
