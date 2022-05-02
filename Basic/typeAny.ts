4645ed4a1acc907f506ac728b8be363a2834c9e1
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-02 09:50:16 +0530

    any type in TS

---
+++ Basic/typeAny.ts
@@ -0,0 +1,15 @@
+
+// You can use OR technic for any 3 of it data can get.
+let data: string | number | boolean = true;
+data="Alex";
+data=10;
+
+// type of "any" you can get any type of variable.
+
+let any:any="Alex";
+any=[1,4,6,"Ram"];
+any={name:"Alex", age:23};
+any=10.340;
+
+
+/* NOTE: Avoid the use of any it creates conflict in your code. */
