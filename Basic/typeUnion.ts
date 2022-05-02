0068c0601c7d69d3fd704a4252399f5258d0c5ae
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-02 10:05:41 +0530

    type union in TS

---
+++ Basic/typeUnion.ts
@@ -0,0 +1,22 @@
+/* Union type is describes a value that can be one of several types */
+
+//Union type is mentioned below.
+
+let data: string | number = 30;
+
+// When use union
+
+function combine(a:number | string, b:number | string){
+    if(typeof a==="number" && typeof b==="number"){
+        return a+b;
+    } else{
+        return a.toString() + b.toString();
+    }
+}
+
+console.log(combine(10,39));
+console.log(combine("Alex", "Bajaj"));
+
+// Also you can use any for here.
+
+/* NOTE : When you don't have any option that time you can use "any" otherwise you have to use "Union" */
