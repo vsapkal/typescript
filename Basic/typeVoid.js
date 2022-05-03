43a2ee37fe58fcb1512c96993bdcd882f1cd3921
Vinod_sapkal78 <vinod.sapkal@infostretch.com>
2022-05-03 10:08:03 +0530

    type void in TS

---
+++ Basic/typeVoid.js
@@ -0,0 +1,12 @@
+/* When you can't return anything in function that time function type is "Void" means return nothing  */
+function printData() {
+    console.log("Hello World");
+}
+printData();
+/* non void function */
+var a = 10;
+var b = 23;
+function combine() {
+    return a + b;
+}
+combine();
---
+++ Basic/typeVoid.ts
@@ -0,0 +1,16 @@
+
+/* When you can't return anything in function that time function type is "Void" means return nothing  */
+function printData(){    // check the data type of "printData". Mouse over on function you see type is "Void". 
+    console.log("Hello World");
+}
+printData();
+
+/* non void function */
+
+let a=10;
+let b=23;
+function combine(){
+    return a+b;
+}
+
+combine();
